e6b68b05173fe62b3007057f828a9e895c50d15a
Michael Volk <volkmchl@gmail.com>
2021-11-16 21:45:51 -0500

    add gitignore

---
+++ Kattis/carrots.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+
+    int n,p;
+    scanf("%d %d", &n, &p);
+    printf("%d\n", p); 
+
+    return 0;
+}
---
+++ Kattis/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    printf("%s", "Hello World!\n");
+    return 0;
+}
